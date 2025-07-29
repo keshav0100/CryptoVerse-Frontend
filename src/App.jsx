ac479818c0becd5b54a4578475fb3eda2061f530
Keshav Bansal <bansalkeshav1390@gmail.com>
2025-07-29 13:35:19 +0530

    Fix import path for Watchlist component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Wallet from "./pages/Wallet/Wallet";
 import Withdrawal from "./pages/Withdrawal/Withdrawal";
 import PaymentDetails from "./pages/Payment Details/PaymentDetails";
 import StockDetails from "./pages/Stock Details/StockDetails";
-import Watchlist from "./pages/Watchlist/watchlist";
+import Watchlist from "./pages/Watchlist/Watchlist";
 import Profile from "./pages/Profile/Profile";
 import SearchCoin from "./pages/Search Coin/SearchCoin";
 import NotFound from "./pages/NotFound/NotFound";
@@ -17,22 +17,24 @@ function App() {
   return (
     <>
       <Auth />
-      {false && <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/activity" element={<Activity />} />
-          <Route path="/wallet" element={<Wallet />} />
-          <Route path="/withdrawal" element={<Withdrawal />} />
-          <Route path="/payment-details" element={<PaymentDetails />} />
-          <Route path="/market/:id" element={<StockDetails />} />
-          <Route path="/watchlist" element={<Watchlist />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/search" element={<SearchCoin />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>}
+      {true && (
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/activity" element={<Activity />} />
+            <Route path="/wallet" element={<Wallet />} />
+            <Route path="/withdrawal" element={<Withdrawal />} />
+            <Route path="/payment-details" element={<PaymentDetails />} />
+            <Route path="/market/:id" element={<StockDetails />} />
+            <Route path="/watchlist" element={<Watchlist />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/search" element={<SearchCoin />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      )}
     </>
   );
 }
